d0f8b9181569a4fc0538c6316bedc93c89f18a1e
Macktireh <abdimack97@gmail.com>
2025-11-08 21:06:15 +0100

    fix: Reduce Uvicorn workers from 4 to 1 for improved resource management

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ app.include_router(sse_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=4)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=1)
